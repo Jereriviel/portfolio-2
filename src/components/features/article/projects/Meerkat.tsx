a7d1aed9dcb719ae077c8e23d79b12a5c7fcd6ed
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-06-03 12:02:48 +0200

    Create project content components for article page

---
+++ src/components/features/article/projects/Meerkat.tsx
@@ -0,0 +1,20 @@
+import TextSection from "../TextSection";
+import TechSection from "../TechSection";
+
+const Meerkat = () => {
+  return (
+    <>
+      <TextSection heading="About" imageUrl="" imageAlt="">
+        <p>About coming soon</p>
+      </TextSection>
+      <TextSection heading="Highlights" imageUrl="" imageAlt="">
+        <p>Highlights coming soon</p>
+      </TextSection>
+      <TechSection>
+        <p>Tech section coming soon</p>
+      </TechSection>
+    </>
+  );
+};
+
+export default Meerkat;
