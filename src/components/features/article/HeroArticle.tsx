7a1edf5a620e8084bdf87caf6bce2f01ada85189
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-06-03 12:01:58 +0200

    Update hero for article page

--- src/components/features/article/HeroArticle.tsx
+++ src/components/features/article/HeroArticle.tsx
@@ -1,4 +1,5 @@
 import type { Project } from "../../../types/project";
+import { Icon } from "@iconify/react";
 
 type HeroArticleProps = {
   project: Project;
@@ -6,10 +7,25 @@ type HeroArticleProps = {
 
 const HeroArticle = ({ project }: HeroArticleProps) => {
   return (
-    <section className="py-8 sm:py-10 md:py-12">
+    <section className="flex flex-col gap-8 py-8 sm:py-10 md:py-12">
       <h1 className="font-display text-accent text-center text-[40px] break-all uppercase sm:text-5xl md:text-6xl">
         {project.title}
       </h1>
+      <img src={project.thumbnail} alt="Thumbnail" />
+      <div className="flex justify-center gap-8 uppercase">
+        <a className="link" target="blank" href={project.liveUrl}>
+          <Icon icon="lucide:globe" />
+          <span>Live</span>
+        </a>
+        <a className="link" target="blank" href={project.repoUrl}>
+          <Icon icon="mdi:github" />
+          <span>Repo</span>
+        </a>
+        <a className="link" target="blank" href={project.figmaUrl}>
+          <Icon icon="solar:figma-bold" />
+          <span>Figma</span>
+        </a>
+      </div>
     </section>
   );
 };
