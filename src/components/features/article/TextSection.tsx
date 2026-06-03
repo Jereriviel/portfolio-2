9880a9c829e19c2c9c471d8c9c1360e19eaf9572
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-06-03 15:25:01 +0200

    Create about and highlight sections for Meerkat project article page

--- src/components/features/article/TextSection.tsx
+++ src/components/features/article/TextSection.tsx
@@ -6,6 +6,7 @@ type TextSectionProps = {
   children: ReactNode;
   imageUrl: string;
   imageAlt: string;
+  reverse?: boolean;
 };
 
 const TextSection = ({
@@ -13,13 +14,22 @@ const TextSection = ({
   children,
   imageUrl,
   imageAlt,
+  reverse,
 }: TextSectionProps) => {
   return (
     <section className="flex w-full flex-col gap-8 py-8 sm:gap-10 sm:py-10 md:gap-12 md:py-12">
       <Heading heading={heading} />
-      <div className="flex flex-col gap-4 sm:gap-6 md:gap-8 lg:gap-12">
+      <div
+        className={`flex flex-col gap-8 lg:gap-12 xl:flex-row ${
+          reverse ? "xl:flex-row-reverse" : ""
+        }`}
+      >
         <div>
-          <img src={imageUrl} alt={imageAlt} className="rounded-lg" />
+          <img
+            src={imageUrl}
+            alt={imageAlt}
+            className="aspect-5/3 rounded-lg object-cover object-top lg:max-w-150"
+          />
         </div>
         <div className="flex flex-col gap-4 sm:gap-6 md:gap-8 md:text-lg lg:flex-1">
           {children}
--- src/components/features/article/projects/Meerkat.tsx
+++ src/components/features/article/projects/Meerkat.tsx
@@ -1,14 +1,46 @@
 import TextSection from "../TextSection";
 import TechSection from "../TechSection";
+import searchImg from "../../../../assets/images/meerkat_search.webp";
+import cartImg from "../../../../assets/images/meerkat_cart.webp";
 
 const Meerkat = () => {
   return (
     <>
-      <TextSection heading="About" imageUrl="" imageAlt="">
-        <p>About coming soon</p>
+      <TextSection heading="About" imageUrl={cartImg} imageAlt="Cart">
+        <p className="md:text-lg">
+          <span className="text-accent">{"{ "}</span>this project was built as a
+          part of my JavaScript Frameworks course at Noroff. The goal was to
+          build a fully functional, responsive online shop with Vite, React,
+          TypeScript and Tailwind, integrated with the Noroff REST API.
+        </p>
+        <p>
+          the application allows users to browse, search and view detailed
+          information about each product, as well as add and remove items from
+          both a shopping cart and a favourites list. The user can adjust
+          quantities and view total cost, and complete a checkout flow. They can
+          also send a message through a validated contact form.
+          <span className="text-accent">{" }"}</span>
+        </p>
       </TextSection>
-      <TextSection heading="Highlights" imageUrl="" imageAlt="">
-        <p>Highlights coming soon</p>
+      <TextSection
+        heading="Highlights"
+        imageUrl={searchImg}
+        imageAlt="Search"
+        reverse={true}
+      >
+        <p className="md:text-lg">
+          <span className="text-accent">{"{ "}</span>fully interactive shopping
+          cart with instant updates, quantity management, persistent state, and
+          a dynamic cart indicator in the header.
+        </p>
+        <p className="md:text-lg">
+          accessible search experience with real-time results and full keyboard
+          navigation support.
+        </p>
+        <p className="md:text-lg">
+          contact forms built with React Hook Form and Zod validation, providing
+          immediate user feedback.<span className="text-accent">{" }"}</span>
+        </p>
       </TextSection>
       <TechSection>
         <p>Tech section coming soon</p>
